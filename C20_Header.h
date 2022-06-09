7a92e6501feeb33f5fbf2e87167d68a78048978b
sray452 <77758012+sray452@users.noreply.github.com>
2022-06-09 14:44:35 -0600

    Add files via upload

---
+++ C20_Header.h
@@ -0,0 +1,31 @@
+#ifndef C20_HEADER_H
+#define C20_HEADER_H
+
+class BinarySearchTree
+{
+    private:
+        struct tree_node
+        {
+           tree_node* left;
+           tree_node* right;
+           int data;
+        };
+        tree_node* root;
+    public:
+        BinarySearchTree()
+        {
+           root = NULL;
+        }
+        bool isEmpty() const { return root==NULL; }
+        void print_inorder();
+        void inorder(tree_node*);
+        void print_preorder();
+        void preorder(tree_node*);
+        void print_postorder();
+        void postorder(tree_node*);
+        void insert(int);
+        void remove(int);
+};
+
+
+#endif // C20_HEADER_H
